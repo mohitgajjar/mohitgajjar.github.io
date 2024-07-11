4a782de545048c84cbc831c7fc85cd6d6ab26dd7
mohitgajjar <131805434+mohitgajjar@users.noreply.github.com>
2024-07-11 13:51:55 +0000

    Mentor

--- assets/css/main.css
+++ assets/css/main.css
@@ -1,6 +1,6 @@
 /**
-* Template Name: Mentor
-* Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
+* Template Name: MK Group
+* Template URL: https://bootstrapmade.com/MK Group-free-education-bootstrap-theme/
 * Updated: Jun 06 2024 with Bootstrap v5.3.3
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
--- assets/scss/Readme.txt
+++ assets/scss/Readme.txt
@@ -1,2 +1,2 @@
 The .scss (Sass) files are only available in the pro version.
-You can buy it from: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
+You can buy it from: https://bootstrapmade.com/MK Group-free-education-bootstrap-theme/
